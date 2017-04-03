b941df2377ab78e582428c7a2f8b0490730806c7
James Small <james.r.small@outlook.com>
2017-04-02 21:43:51 -0400

    Update site code to current - synced from FSND-WebDocs/Project1/src

---
+++ js/modal.js
@@ -0,0 +1,25 @@
+// Get the modal
+var modal = document.getElementById('descrModal');
+
+// Image setup
+var img = document.getElementById('fpeModal');
+
+// When the user clicks on the image, open the modal
+img.onclick = function() {
+    modal.style.display = "block";
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
